89fc0c63dcf2643893acc4f7504bf93f783fe103
Daniel <danielm.wenz@gmail.com>
2019-05-21 09:37:55 -0300

    includes desnecessarios em dashboardcontroller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
-use Illuminate\Http\Request;
-use Spatie\Permission\Models\Permission;
-use Spatie\Permission\Models\Role;
 
 class DashboardController extends Controller
 {
