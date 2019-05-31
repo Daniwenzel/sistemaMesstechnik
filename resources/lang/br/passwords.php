0095cceffcec9b81544ea9b2ff5b057c4adb5f31
Daniel <danielm.wenz@gmail.com>
2019-05-31 09:24:23 -0300

    traducao de mensagens para portugues

---
+++ resources/lang/br/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Senhas devem conter pelo menos seis caracteres e combinar com a confirmação.',
+    'reset' => 'A sua senha foi redefinida!',
+    'sent' => 'Enviamos um e-mail com o link para redefinição de senha!',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => "Não conseguimos achar um usuário com este endereço de e-mail.",
+
+];
