66d100358d87fdaf9e023c2640edd2533b0207a5
Daniel <danielm.wenz@gmail.com>
2019-06-07 09:26:50 -0300

    Mudança nos textos alts, remoção de icones do navbar, rota alterar senha

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,10 +1,10 @@
 <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
         <a class="navbar-brand brand-logo" href="{{ route('dashboard') }}">
-            <img src="{{ asset('images/logo.png') }}" alt="logo" />
+            <img src="{{ asset('images/logo.png') }}" alt="Imagem logo" />
         </a>
         <a class="navbar-brand brand-logo-mini" href="{{ route('dashboard') }}">
-            <img class="img-xs rounded-circle" src="{{ asset('images/auth/logo.png') }}" alt="Profile image">
+            <img class="img-xs rounded-circle" src="{{ asset('images/auth/logo.png') }}" alt="Imagem perfil">
         </a>
     </div>
     <div class="navbar-menu-wrapper d-flex align-items-center ">
@@ -13,29 +13,16 @@
                 <a class="nav-link dropdown-toggle d-none d-xl-inline-block" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
                     <span class="profile-text">Olá, {{ Auth::user()->name  }} !</span>
                     @if(Auth::user()->getFirstMedia('profile'))
-                        <img class="img-xs rounded-circle" src="{{ Auth::user()->getFirstMedia('profile')->getUrl('avatar') }}" alt="profile image">
+                        <img class="img-xs rounded-circle" src="{{ Auth::user()->getFirstMedia('profile')->getUrl('avatar') }}" alt="Imagem perfil">
                     @else
-                        <img class="img-xs rounded-circle" src="{{ asset('images/faces-clipart/pic-1.png') }}" alt="profile image">
+                        <img class="img-xs rounded-circle" src="{{ asset('images/faces-clipart/pic-1.png') }}" alt="Imagem perfil">
                     @endif
                 </a>
                 <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
-                    <a class="dropdown-item p-0">
-                        <div class="d-flex border-bottom">
-                            <div class="py-3 px-4 d-flex align-items-center justify-content-center">
-                                <i class="mdi mdi-bookmark-plus-outline mr-0 text-gray"></i>
-                            </div>
-                            <div class="py-3 px-4 d-flex align-items-center justify-content-center border-left border-right">
-                                <i class="mdi mdi-account-outline mr-0 text-gray"></i>
-                            </div>
-                            <div class="py-3 px-4 d-flex align-items-center justify-content-center">
-                                <i class="mdi mdi-alarm-check mr-0 text-gray"></i>
-                            </div>
-                        </div>
-                    </a>
                     <a class="dropdown-item mt-2" href="{{ route('show.config', Auth::user()->id) }}">
                         Manage Account
                     </a>
-                    <a class="dropdown-item">
+                    <a class="dropdown-item" href="{{ route('user.password') }}">
                         Change Password
                     </a>
 
