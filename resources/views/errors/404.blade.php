9ff0b678f82c2f839c2e3538d3262371b6de835a
Daniel <danielm.wenz@gmail.com>
2019-07-03 08:17:33 -0300

    Atualizando assets nas views de erros (403,404,500)

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,26 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="Description" content="Company: Messtechnik, Product: Wind measurement farms,
+    monitoring wind conditions, anemometric tower, anemometro, barometro, windvane, datalogger,
+     Location: Brazil">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Sistema Messtechnik') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
 
     <!-- Styles -->
-    <link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css">
-    <link href="{{asset('css/feather.css')}}" rel="stylesheet" type="text/css">
-    <link href="{{asset('css/materialdesignicons.min.css')}}" rel="stylesheet" type="text/css">
-
-    <link href="{{asset('css/vendor.bundle.addons.css')}}" rel="stylesheet" type="text/css">
-    <link href="{{asset('css/vendor.bundle.base.css')}}" rel="stylesheet" type="text/css">
-
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css">
 </head>
 
 <body>
@@ -55,11 +51,5 @@
     </div>
     <!-- page-body-wrapper ends -->
 </div>
-
-<script src="{{ asset('js/app.js') }}" type="text/javascript" defer></script>
-<script src="{{ asset('js/off-canvas.js') }}" type="text/javascript" defer></script>
-<script src="{{ asset('js/misc.js') }}" type="text/javascript" defer></script>
-<script src="{{ asset('js/dashboard.js') }}" type="text/javascript" defer></script>
-
 </body>
 </html>
