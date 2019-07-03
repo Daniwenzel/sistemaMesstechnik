e3559704e2cc7f27950e477012c8c3486b32d4d1
Daniel <danielm.wenz@gmail.com>
2019-07-03 10:04:46 -0300

    Biblioteca de acesso ao server SMB e re-organização do laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,20 +11,17 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js/app.js')
-    .js('resources/js/inline.js', 'public/js/inline.js')
-    .sass('vendor/iconfonts/font-awesome/scss/font-awesome.scss', 'public/css')
-    .copy('resources/js/alerts.js', 'public/js')
-    .copy('resources/js/chart.js', 'public/js')
-    .copy('resources/js/password.js', 'public/js')
-    .copy('resources/js/login.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/inline.js', 'public/js')
     .sass('resources/scss/style.scss', 'public/css')
+    .sass('node_modules/font-awesome/scss/font-awesome.scss', 'public/css')
     .sass('node_modules/sweetalert2/src/sweetalert2.scss', 'public/css')
-    .sass('node_modules/bootstrap/scss/bootstrap.scss', 'public/css/bootstrap.css')
-    .styles(['vendor/iconfonts/mdi/css/materialdesignicons.min.css',
-        'vendor/iconfonts/puse-icons-feather/feather.css',
-        'vendor/css/vendor.bundle.base.css',
-        'vendor/css/vendor.bundle.addons.css',
-        'node_modules/animate.css/animate.min.css'], 'public/css/all.css')
-    .copy(['node_modules/@mdi/font/fonts'], 'public/fonts')
+    .sass('node_modules/@mdi/font/scss/materialdesignicons.scss', 'public/css')
+    .styles([
+        'public/css/font-awesome.css',
+        'public/css/sweetalert2.css',
+        'public/css/materialdesignicons.css'
+    ], 'public/css/all.css')
+    .copy('node_modules/animate.css/animate.min.css', 'public/css')
+    .copy('resources/js/chart.js', 'public/js')
     .copy('resources/images', 'public/images');
