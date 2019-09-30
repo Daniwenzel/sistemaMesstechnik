9198a0e94ffabfe691ea0f924a57f64cd0efe327
Daniel <danielm.wenz@gmail.com>
2019-09-30 15:49:18 -0300

    Configurar imagem de usuário

---
+++ public/js/image.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $("#image-upload").click(function () {
+        $.ajax({
+            url: '/user/avatar',
+            type: 'POST',
+
+            // Form data
+            data: new FormData($('#avatar-form')[0]),
+
+            // Tell jQuery not to process data or worry about content-type
+            // You *must* include these options!
+            cache: false,
+            contentType: false,
+            processData: false,
+
+            // Custom XMLHttpRequest
+            xhr: function () {
+                var myXhr = $.ajaxSettings.xhr();
+                if (myXhr.upload) {
+                    // For handling the progress of the upload
+                    myXhr.upload.addEventListener('progress', function (e) {
+                        if (e.lengthComputable) {
+                            $('progress').attr({
+                                value: e.loaded,
+                                max: e.total,
+                            });
+                        }
+                    }, false);
+                }
+                return myXhr;
+            }
+        });
+    });
+});
--- resources/views/userconfig.blade.php
+++ resources/views/userconfig.blade.php
@@ -7,7 +7,7 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">{{ __('labels.registration', ['attribute' => $user->name]) }}</h4>
-                        <form method="POST" action="{{ route('edit.config', $user->id) }}" class="mt-5">
+                        <form method="POST" action="{{ route('users.update', $user->id) }}" class="mt-5">
                             @csrf
 
                             <div class="form-group row">
@@ -66,20 +66,35 @@
                             </div>
                             @role('Admin')
                             <div class="form-group row mt-5">
-                                <div class="col-md-6">
+                                <div class="col-md-4">
                                     <div class="form-radio">
                                         <label class="form-check-label">
-                                            <input type="radio" class="form-check-input" name="accountRole" id="accountRoleBasic" value="Basica" {{ $role === 'Basica' ? 'checked' : '' }}> {{ __('labels.basic') }}
+                                            <input type="radio" class="form-check-input" name="accountRole"
+                                                   id="accountRoleBasic" value="Basica" {{ $role === 'Basica' ? 'checked' : '' }}
+                                                    {{ $role === 'Admin' ? 'disabled' : '' }}> {{ __('labels.basic') }}
                                         </label>
                                     </div>
                                 </div>
-                                <div class="col-md-6">
+                                <div class="col-md-4">
                                     <div class="form-radio">
                                         <label class="form-check-label">
-                                            <input type="radio" class="form-check-input" name="accountRole" id="accountRoleMaster" value="Master" {{ $role === 'Master' ? 'checked' : '' }}> {{ __('labels.master') }}
+                                            <input type="radio" class="form-check-input" name="accountRole"
+                                                   id="accountRoleMaster" value="Master" {{ $role === 'Master' ? 'checked' : '' }}
+                                                    {{ $role === 'Admin' ? 'disabled' : '' }}> {{ __('labels.master') }}
                                         </label>
                                     </div>
                                 </div>
+                                @if($user->hasRole('Admin'))
+                                    <div class="col-md-4">
+                                        <div class="form-radio">
+                                            <label class="form-check-label">
+                                                <input type="radio" class="form-check-input" name="accountRole"
+                                                       id="accountRoleAdmin" value="Admin" {{ $role === 'Admin' ? 'checked' : '' }}>
+                                                {{ __('labels.admin') }}
+                                            </label>
+                                        </div>
+                                    </div>
+                                @endif
                             </div>
                             @endrole
                             <div class="form-group row">
@@ -87,15 +102,13 @@
                                     <i class="mdi mdi-file-document m-2"></i>Submit</button>
                             </div>
                         </form>
-                        <form class="mt-5" method="POST" action="{{ route('edit.avatar', $user->id) }}" enctype="multipart/form-data">
-                            @csrf
-
+                        <form enctype="multipart/form-data" id="avatar-form">
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group row">
                                         <label for="avatar" class="col-sm-3 col-form-label">{{ __('Avatar (opcional)') }}</label>
                                         <div class="col-sm-9">
-                                            <input type="file" class="form-control file-upload-info file-upload-browse btn btn-outline-success" name="avatar" />
+                                            <input type="file" name="avatar" class="form-control file-upload-info file-upload-browse btn btn-outline-success"/>
                                             @if ($errors->has('avatar'))
                                                 <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $errors->first('avatar') }}</strong>
@@ -105,8 +118,7 @@
                                     </div>
                                 </div>
                                 <div class="col-md-6">
-                                    <button type="submit" class="btn btn-outline-info btn-fw">
-                                        <i class="mdi mdi-upload"></i>Upload</button>
+                                    <input type="button" id="image-upload" value="Upload" class="btn btn-outline-info btn-fw" />
                                 </div>
                             </div>
                             <div class="row mt-5">
@@ -119,6 +131,37 @@
                                 </div>
                             </div>
                         </form>
+{{--                        <form class="mt-5" method="POST" action="{{ route('edit.avatar', $user->id) }}" enctype="multipart/form-data">--}}
+{{--                            @csrf--}}
+{{--                            <div class="row">--}}
+{{--                                <div class="col-md-6">--}}
+{{--                                    <div class="form-group row">--}}
+{{--                                        <label for="avatar" class="col-sm-3 col-form-label">{{ __('Avatar (opcional)') }}</label>--}}
+{{--                                        <div class="col-sm-9">--}}
+{{--                                            <input type="file" class="form-control file-upload-info file-upload-browse btn btn-outline-success" name="avatar" />--}}
+{{--                                            @if ($errors->has('avatar'))--}}
+{{--                                                <span class="invalid-feedback" role="alert">--}}
+{{--                                                <strong>{{ $errors->first('avatar') }}</strong>--}}
+{{--                                            </span>--}}
+{{--                                            @endif--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                                <div class="col-md-6">--}}
+{{--                                    <button type="submit" class="btn btn-outline-info btn-fw">--}}
+{{--                                        <i class="mdi mdi-upload"></i>Upload</button>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <div class="row mt-5">--}}
+{{--                                <div class="col-md-12">--}}
+{{--                                    <div class="form-group row justify-content-md-center">--}}
+{{--                                        @if($user->getFirstMedia('profile'))--}}
+{{--                                            <img src="{{ $user->getFirstMedia('profile')->getUrl('preview') }}" alt="profile_image_preview"/>--}}
+{{--                                        @endif--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </form>--}}
                     </div>
                 </div>
             </div>
