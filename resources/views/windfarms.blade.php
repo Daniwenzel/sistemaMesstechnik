34fea376b11295d1ed9ddcb8e3fa0b445f3ea95b
Daniel <danielm.wenz@gmail.com>
2019-09-30 15:44:51 -0300

    Filtro de torres para a empresa selecionada

--- resources/views/windfarms.blade.php
+++ resources/views/windfarms.blade.php
@@ -3,12 +3,23 @@
 @section('content')
     <div data-barba-namespace="windfarms">
         @role('Admin')
-        <button type="button" class="btn btn-outline-primary btn-md btn-block" onclick="window.location=
-                '{{ route('show.register.windfarm') }}'">
-            <i class="mdi mdi-wind-turbine mdi-24px"></i>{{ __('buttons.register_windfarm') }}
-        </button>
+        <form method="get" action="{{ route('windfarms.create') }}">
+            <button type="submit" class="btn btn-outline-primary btn-md btn-block">
+                <i class="mdi mdi-wind-turbine mdi-24px"></i>{{ __('buttons.register_windfarm') }}
+            </button>
+        </form>
+        <form method="get" action="{{ route('windfarms.index') }}" class="navbar-form navbar-left mt-3">
+            <div class="form-group">
+                <select class="form-control mt-3" id="empresa" name="empresa" onchange="this.form.submit();">
+                    <option {{ $empresa_selecionada == 'all' ? 'selected' : ''}} value="all">Todas</option>
+                    @foreach($empresas as $empresa)
+                        <option {{ $empresa_selecionada == $empresa->id ? 'selected' : '' }} value="{{ $empresa->id }}">{{ $empresa->nome }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </form>
         @endrole
-        <form method="get" action="{{ route('windfarm') }}" class="navbar-form navbar-left mt-4">
+        <form method="get" action="{{ route('windfarms.index') }}" class="navbar-form navbar-left mt-3">
             <div class="form-group">
                 <div class="input-group">
                     <input name="search" type="text" class="form-control" placeholder="{{ __('labels.search') }}" aria-label="Username" aria-describedby="colored-addon3">
@@ -24,7 +35,7 @@
             <div class="row text-md-center">
                 <div class="col-md-12 stretch-card grid-margin">
                     <div class="card bg-gradient-info card-img-holder text-white card-button">
-                        <div class="card-body btn" onclick="window.location='{{ route('windfarm.info', $windfarm->id) }}'">
+                        <div class="card-body btn" onclick="window.location='{{ route('windfarms.show', $windfarm->id) }}'">
                             <img src="{{ asset('images/circle.svg') }}" class="card-img-absolute" alt="circle-image"/>
                             <img src="{{ asset('images/wind-turbine.svg') }}" class="card-img-absolute" alt="wind-turbine"/>
                             <h4 class="font-weight-normal mb-3">{{ $windfarm->cod_EPE }}</h4>
@@ -35,7 +46,7 @@
             </div>
         @endforeach
         <div class="row justify-content-md-center">
-            {{ $windfarms->appends(['search' => $filtro])->links() }}
+            {{ $windfarms->appends(['search' => $filtro, 'empresa' => $empresa_selecionada])->links() }}
         </div>
     </div>
 @endsection
