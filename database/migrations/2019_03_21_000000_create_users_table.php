b1416758399adbd1918abd7aafd4b6ff7710f0cc
Daniel <danielm.wenz@gmail.com>
2020-03-23 15:33:48 -0300

    Alteracoes migrations

--- database/migrations/2019_03_21_000000_create_users_table.php
+++ database/migrations/2019_03_21_000000_create_users_table.php
@@ -3,49 +3,27 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-//use Illuminate\Support\Facades\DB;
-//use Messtechnik\User;
 
 class CreateUsersTable extends Migration
 {
     public function up()
     {
-        Schema::create('USERS', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('last_name')->nullable();
             $table->string('email')->unique();
-            $table->string('genero')->nullable();
-            $table->date('aniversario')->nullable();
+            $table->date('birthday')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('cliente_codigo');
+            $table->integer('clicodigo');
             $table->rememberToken();
             $table->timestamps();
-
-            $table->foreign('cliente_codigo')
-                ->references('CODIGO')
-                ->on('CLIENTE');
         });
 
-       // $empresa = DB::select('select * from cliente where razaosocial=?', ['TESTE']);
-
-//        dd($empresa[0]->codigo);
-
-//        $usuario = new User([
-//            'id' => 1,
-//            'name' => 'Admin',
-//            'email' => 'admin@admin',
-//            'password' => bcrypt('mstk123'),
-//            'cliente_codigo' => 1,
-//            'created_at' => now(),
-//            'updated_at' => now()
-//        ]);
-//        $user->save();
     }
 
     public function down()
     {
-        Schema::dropIfExists('USERS');
+        Schema::dropIfExists('users');
     }
 }
