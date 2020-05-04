2367a855930f6eb65d87747082aab5ed0313bc5e
Daniel <danielm.wenz@gmail.com>
2020-05-04 08:35:28 -0300

    Adição script EPE e atualização dos demais scripts Rcode

---
+++ resources/rcode/scriptCompare.R
@@ -0,0 +1,152 @@
+library(odbc)
+library(DBI)
+library(tidyverse)
+library(plotly)
+library(lubridate)
+library(chron)
+
+args <- commandArgs(trailingOnly = TRUE)
+codigoEstacaoPrimeiraTorre <- args[1]
+codigoEstacaoSegundaTorre <- args[2]
+dataInicio <- args[3]
+dataFim <- args[4]
+
+#codigoEstacaoPrimeiraTorre <- '000579'
+#codigoEstacaoSegundaTorre <- '000581'
+#dataInicio <- '2019-06-15'
+#dataFim <- '2019-06-30'
+
+con <- dbConnect(odbc::odbc(),dsn='measures')
+#con <- DBI::dbConnect(odbc::odbc(),
+#                      driver='Firebird/InterBase(r) driver',
+#                      dbname='measures',
+#                      user='sysdba',
+#                      password='masterkey',
+#                      host='localhost',
+#                      port=3050)
+
+res <- dbSendQuery(con, "SELECT * FROM SITE sit WHERE sit.ESTACAO=?")
+dbBind(res, c(codigoEstacaoPrimeiraTorre))
+primeiraTorre <- dbFetch(res)
+dbBind(res, c(codigoEstacaoSegundaTorre))
+segundaTorre <- dbFetch(res)
+dbClearResult(res)
+
+dias <- seq(from = ymd(dataInicio), to = ymd(dataFim), by='days')
+
+qryMeasures <- paste0("SELECT DTAREG, REGVALUE FROM REGDATA WHERE SITCODIGO = ? AND ", 
+                        "FLDCODIGO = ? AND DTAREG between '",dataInicio," 00:00:00' and '",
+                        dataFim," 23:50:00' ORDER BY DTAREG ASC")
+
+horaminuto <- merge(0:23, seq(0, 50, by = 10))
+datetime <- merge(dias, chron(time = paste(horaminuto$x, ':', horaminuto$y, ':', 0)))
+datetime$dt <- as.POSIXct(paste(datetime$x, datetime$y))
+datetime <- datetime[order(datetime$dt), ]
+datetime$dt <- as.character(round_date(datetime$dt, "minute"))
+
+row.names(datetime) <- NULL
+
+# Seleciona o maior código VRS (o último measurement adicionado) das torres escolhidas
+res <- dbSendQuery(con, "SELECT MAX(CODIGO) FROM CFGVRS WHERE SITCODIGO=?")
+dbBind(res, primeiraTorre$CODIGO)
+primeiraTorre["VRSCODIGO"] <- dbFetch(res)
+dbBind(res, segundaTorre$CODIGO)
+segundaTorre["VRSCODIGO"] <- dbFetch(res)
+dbClearResult(res)
+
+res <- dbSendQuery(con, "SELECT med.FLDCODIGO,fld.FLDNAME FROM MEDICAO med JOIN CFGFLD fld ON med.FLDCODIGO=fld.CODIGO WHERE med.VRSCODIGO=?")
+dbBind(res, primeiraTorre$VRSCODIGO)
+fldCodigosPrimeira <- dbFetch(res)
+dbBind(res, segundaTorre$VRSCODIGO)
+fldCodigosSegunda <- dbFetch(res)
+dbClearResult(res)
+
+
+dataPrimeira <- data.frame(datetime$dt)
+names(dataPrimeira) <- c("DTAREG")
+missingPrimeira <- data.frame()
+
+# Para cada sensor da primeira torre, busca pelas leituras
+for (i in 1:nrow(fldCodigosPrimeira)) {
+  res <- dbSendQuery(con, qryMeasures)
+  dbBind(res, c(primeiraTorre$CODIGO, fldCodigosPrimeira[i,1]))
+  sensor <- dbFetch(res)
+  # Removendo os milisegundos da data
+  sensor$DTAREG <- as.character(round_date(sensor$DTAREG, "minute"))
+  # Nomeando a coluna com o nome do sensor
+  names(sensor) <- c("DTAREG", fldCodigosPrimeira[i,2])
+  
+  dataPrimeira <- merge(dataPrimeira,sensor,by="DTAREG",all=TRUE)  
+  
+  #missingPrimeira <- merge(missingPrimeira,dataPrimeira$DTAREG[is.na(dataPrimeira[,ncol(dataPrimeira)])])
+  
+  rm(sensor)
+  dbClearResult(res)
+}
+
+dataSegunda <- data.frame(datetime$dt)
+names(dataSegunda) <- c("DTAREG")
+
+#for (i in (1:nrow(canaisSegunda))[!is.na(canaisSegunda$FLDCODIGO)]) {
+for (i in 1:nrow(fldCodigosSegunda)) {
+  res <- dbSendQuery(con, qryMeasures)
+  dbBind(res, c(segundaTorre$CODIGO,fldCodigosSegunda[i,1]))
+  sensor <- dbFetch(res)
+  # Removendo os milisegundos da data
+  sensor$DTAREG <- as.character(round_date(sensor$DTAREG, "minute"))
+  # Nomeando a coluna com o nome do sensor
+  names(sensor) <- c("DTAREG", fldCodigosSegunda[i,2])
+
+  dataSegunda <- merge(dataSegunda,sensor,by="DTAREG",all=TRUE)  
+
+  rm(sensor)
+  dbClearResult(res)
+}
+
+not_all_na <- function(x) any(!is.na(x))
+sensoresPrimeiraAvg <- dataPrimeira %>% dplyr::select(ends_with("Avg")) %>% select_if(not_all_na)
+sensoresSegundaAvg <- dataSegunda %>% dplyr::select(ends_with("Avg")) %>% select_if(not_all_na)
+
+relacao <- data.frame(sensor=names(sensoresSegundaAvg), altura=as.numeric(gsub("_|-",".",gsub("AN_|WV_|B_|T_|U_|_Avg","",names(sensoresSegundaAvg)))) )
+
+nomePrimeiraTorre <- str_extract(string = primeiraTorre$SITENAME, pattern = "\\([^()]+\\)")
+nomePrimeiraTorre <- substring(nomePrimeiraTorre,2,nchar(nomePrimeiraTorre)-1)
+nomeSegundaTorre <- str_extract(string = segundaTorre$SITENAME, pattern = "\\([^()]+\\)")
+nomeSegundaTorre <- substring(nomeSegundaTorre,2,nchar(nomeSegundaTorre)-1)
+
+dir <- paste0("/var/www/sistemaMesstechnik/public/images/plots/")
+plotsDir <- paste0(dir,primeiraTorre$ESTACAO,"-",segundaTorre$ESTACAO)
+dir.create(file.path(plotsDir), showWarnings = FALSE)
+
+for (i in 1:ncol(sensoresPrimeiraAvg)) {
+  # extrai a substring até o caractere '_' ou '-' e atribui o resultado ao tipo de sensor
+  tipoSensor <- sub("_.*|-.*", "", names(sensoresPrimeiraAvg)[i])
+  alturaSensor <- as.numeric(gsub("_|-",".",gsub(paste0(tipoSensor,"|Avg|_"),"",names(sensoresPrimeiraAvg[i]))))
+  sensoresEncontrados <- dplyr::filter(relacao, grepl(tipoSensor, sensor))
+  
+  sensorPar <- as.character(sensoresEncontrados$sensor[first(which(abs(sensoresEncontrados$altura-alturaSensor)==min(abs(sensoresEncontrados$altura-alturaSensor))))])
+
+  tituloPlot <- sub("AN","Anemometros",sub("WV","Windvanes",sub("B","Barometros",sub("T","Temperaturas",sub("U","Umidades",tipoSensor)))))
+  legendaPrimeiraTorre <- paste0(nomePrimeiraTorre,": ",names(sensoresPrimeiraAvg[i]))
+  legendaSegundaTorre <- paste0(nomeSegundaTorre,": ",sensorPar)
+  
+  plot <- ggplot() + 
+    geom_line(data = dataPrimeira,aes(x=as.POSIXct(DTAREG), y=dataPrimeira[,names(sensoresPrimeiraAvg[i])],colour=legendaPrimeiraTorre),size=0.3) +
+    geom_line(data = dataSegunda,aes(x=as.POSIXct(DTAREG), y=dataSegunda[,sensorPar],colour=legendaSegundaTorre),size=0.3) +
+    scale_colour_manual("", 
+                        breaks = c(legendaPrimeiraTorre,legendaSegundaTorre),
+                        values = c("#56B4E9", "#E3265C")) +
+    scale_x_datetime(date_breaks = "12 hours" , date_labels = "%d/%b %R") +
+    labs(title=tituloPlot,
+         x='',y='') +
+    theme(axis.text.x=element_text(size=5, angle=45, hjust=1),
+          legend.position="bottom",
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.line = element_line(colour = "black"))
+    
+  ggsave(file = paste0(tituloPlot,".png"), plot = plot, device = "png", path = plotsDir, height = 4, width = 8)
+}
+
+dbDisconnect(con)
---
+++ resources/rcode/scriptGenerate.R
@@ -0,0 +1,110 @@
+library(odbc)
+library(tidyverse)
+library(plotly)
+library(lubridate)
+library(chron)
+library(clifro)
+
+
+args <- commandArgs(trailingOnly = TRUE)
+codigoEstacaoPrimeiraTorre <- args[1]
+dataInicio <- args[2]
+dataFim <- args[3]
+
+#codigoEstacaoPrimeiraTorre <- '000579'
+#dataInicio <- '2019-06-15'
+#dataFim <- '2019-06-30'
+
+
+con <- dbConnect(odbc::odbc(),dsn='measures')
+
+primeiraTorre <- dbGetQuery(con, paste0("SELECT * FROM SITE sit WHERE sit.ESTACAO='",codigoEstacaoPrimeiraTorre,"'"))
+
+dias <- seq(from = ymd(dataInicio), to = ymd(dataFim), by='days')
+
+qryMeasures <- paste0("SELECT DTAREG, REGVALUE FROM REGDATA WHERE SITCODIGO = ",primeiraTorre$CODIGO, 
+                        " AND FLDCODIGO = ? AND DTAREG between '",dataInicio," 00:00:00' and '",
+                        dataFim," 23:50:00' ORDER BY DTAREG ASC")
+
+horaminuto <- merge(0:23, seq(0, 50, by = 10))
+datetime <- merge(dias, chron(time = paste(horaminuto$x, ':', horaminuto$y, ':', 0)))
+datetime$dt <- as.POSIXct(paste(datetime$x, datetime$y))
+datetime <- datetime[order(datetime$dt), ]
+datetime$dt <- as.character(round_date(datetime$dt, "minute"))
+
+row.names(datetime) <- NULL
+
+# Seleciona o maior código VRS (o último measurement adicionado) das torres escolhidas
+primeiraTorre["VRSCODIGO"] <- dbGetQuery(con, paste0("SELECT MAX(CODIGO) FROM CFGVRS WHERE SITCODIGO=",primeiraTorre$CODIGO))
+
+fldCodigosPrimeira <- dbGetQuery(con, paste0("SELECT med.FLDCODIGO,fld.FLDNAME FROM MEDICAO med JOIN CFGFLD fld ON med.FLDCODIGO=fld.CODIGO WHERE med.VRSCODIGO=",primeiraTorre$VRSCODIGO))
+
+dataPrimeira <- data.frame(datetime$dt)
+names(dataPrimeira) <- c("DTAREG")
+#missingPrimeira <- data.frame()
+
+# Para cada sensor da primeira torre, busca pelas leituras
+for (i in 1:nrow(fldCodigosPrimeira)) {
+  res <- dbSendQuery(con, qryMeasures)
+  dbBind(res, c(fldCodigosPrimeira[i,1]))
+  sensor <- dbFetch(res)
+  # Removendo os milisegundos da data
+  sensor$DTAREG <- as.character(round_date(sensor$DTAREG, "minute"))
+  # Nomeando a coluna com o nome do sensor
+  names(sensor) <- c("DTAREG", fldCodigosPrimeira[i,2])
+  
+  dataPrimeira <- merge(dataPrimeira,sensor,by="DTAREG",all=TRUE)  
+  
+  #missingPrimeira <- merge(missingPrimeira,dataPrimeira$DTAREG[is.na(dataPrimeira[,ncol(dataPrimeira)])])
+  
+  rm(sensor)
+  dbClearResult(res)
+}
+
+nomePrimeiraTorre <- str_extract(string = primeiraTorre$SITENAME, pattern = "\\([^()]+\\)")
+nomePrimeiraTorre <- substring(nomePrimeiraTorre,2,nchar(nomePrimeiraTorre)-1)
+
+not_all_na <- function(x) any(!is.na(x))
+sensoresAvg <- dataPrimeira %>% dplyr::select(ends_with("Avg")) %>% select_if(not_all_na) %>% add_column(DTAREG=dataPrimeira$DTAREG, .before=1)
+
+dir <- paste0("/var/www/sistemaMesstechnik/public/images/plots/")
+plotsDir <- paste0(dir,primeiraTorre$ESTACAO)
+dir.create(file.path(plotsDir), showWarnings = FALSE)
+
+for(i in 2:ncol(sensoresAvg)) {
+  plot <- ggplot() + 
+    geom_line(data = sensoresAvg, aes(x=as.POSIXct(DTAREG), y=sensoresAvg[,i],colour=nomePrimeiraTorre),size=0.3) +
+    scale_colour_manual("", 
+                        breaks = c(nomePrimeiraTorre),
+                        values = c("#56B4E9")) +
+    scale_x_datetime(date_breaks = "12 hours" , date_labels = "%d/%b %R") +
+    labs(title=names(sensoresAvg[i]),
+         x=' ',
+         y=' ') +
+    theme(axis.text.x=element_text(size=8, angle=45, hjust=1),
+          legend.position="bottom",
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.line = element_line(colour = "black"))
+  ggsave(file = paste0(names(sensoresAvg[i]),".png"), plot = plot, device = "png", path = plotsDir, height = 4, width = 8)
+}
+
+windvanes <- dataPrimeira %>% dplyr::select(ends_with("Avg")) %>% dplyr::select(starts_with("WV"))
+anemometros <- dataPrimeira %>% dplyr::select(ends_with("Avg")) %>% dplyr::select(starts_with("AN"))
+
+wvAlturas <- as.numeric(gsub("_|-",".",gsub("WV_|_Avg","",names(windvanes))))
+anAlturas <- as.numeric(gsub("_|-",".",gsub("AN_|_Avg","",names(anemometros))))
+
+for (i in 1:length(wvAlturas)) {
+  anemometroPar <- anemometros[first(which(abs(anAlturas-wvAlturas[i])==min(abs(anAlturas-wvAlturas[i]))))]
+  
+  windrose <- windrose(speed = as.numeric(unlist(anemometroPar)),
+                       direction = as.numeric(unlist(windvanes[i])),
+                       speed_cuts = seq(0,25,5),
+                       legend_title="Velocidades [m/s]",
+                       ggtheme='minimal')+labs(title=names(windvanes[i]))
+  ggsave(file = paste0("rosaventos-",i,".png"), plot = windrose, device = "png", path = plotsDir, height = 4, width = 8)
+}
+
+dbDisconnect(con)
